71917e8e821dede81a2d401a973a302d4acdc6ce
fallwith <fallwith@gmail.com>
2022-10-19 13:00:05 -0700

    set a fake env var in testing
    to help make sure real GitHub API calls are not performed by the tests,
    set (and potentially overwrite) the token env var

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,3 +6,5 @@ $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'stoat'
 
 require 'minitest/autorun'
+
+ENV[Stoat::Helpers::TOKEN_ENV_VAR] = 'fake-token-to-help-prevent-real-github-api-calls'
